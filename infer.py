6ccfb5dcc82b443f0e2ecc2bc37fb458a2fb3448
Abhishek <48456430+abhionic@users.noreply.github.com>
2025-11-04 10:52:12 -0500

    14m

--- infer.py
+++ infer.py
@@ -20,14 +20,14 @@ for message in st.session_state.messages:
 # load the model once and use it across all users and sessions
 @st.cache_resource
 def load_model():
-    return kagglehub.model_download('abhionic/medcon/keras/3m')
+    return kagglehub.model_download('abhionic/medcon/keras/14m')
 
-template = 'Patient: {patient} Doctor: {doctor}'
+template = '<User> {user} <Model> '
 path = load_model()
 model = keras.saving.load_model(f'{path}/model.keras')
 vocab = f'{path}/vocab.txt'; seq_len = 256
 tokenizer = kh.tokenizers.WordPieceTokenizer(vocab, seq_len, lowercase=True)
-sampler = kh.samplers.TopPSampler(p=0.1, k=5)
+sampler = kh.samplers.TopPSampler(temperature=0.2, p=0.5, k=5)
 def next(prompt, cache, index):
     logits = model(prompt)[:, index-1, :]; hidden_states = None
     return logits, hidden_states, cache
@@ -40,7 +40,7 @@ if prompt := st.chat_input('please enter your symptoms'):
     with st.chat_message('user'):
         st.markdown(prompt)
 
-    text = template.format(patient=prompt, doctor='')
+    text = template.format(user=prompt)
     tokens = tokenizer(text); tokens = ops.expand_dims(tokens, axis=0)
     ct = ops.count_nonzero(tokens)
     outokens = sampler(next=next, prompt=tokens, index=ct)
